6e66409b2418ea8276ab349a1426ac0903456a2c
Maarit <ylitalo.maarit@gmail.com>
2023-01-16 10:37:02 +0200

    Code is writted

--- robot_for_python.py
+++ robot_for_python.py
@@ -1,13 +1,23 @@
-
 # Robot for Python
 
+# Imports
 import serial
 import time
 
 try:
-	print("Arduino is connected.")
-	arduino_connection = serial.Serial("/dev/cu.usbserial-1420", 9600)
-	arduino_connection.close()
+	ser = serial.Serial("/dev/cu.HC-06")  # Bluetooth is connected
+	ser.write(b's')
+	ser.write(b'k')
+	while True:
+		test_input = input("BODY: ").strip()  # Robot's body
+		if test_input == 'a':
+			ser.write(b'a')  # Robot's head is turning left
+		elif test_input == 's':
+			ser.write(b's')  # Robot's head is turning center
+		elif test_input == 'd':
+			ser.write(b'd')  # Robot's head is turning right
 except Exception:
-	print("Arduino is not connected.")
+	# Disconnected
+	print("Arduino isn't connected.")
+
 
