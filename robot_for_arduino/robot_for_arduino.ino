57083a48c50d918aea6b8f5ec5a7f11a5da18784
Maarit <ylitalo.maarit@gmail.com>
2023-01-17 18:17:55 +0200

    Arduino's file is added

---
+++ robot_for_arduino/robot_for_arduino.ino
@@ -0,0 +1,93 @@
+
+// LIBRARIES
+#include <Servo.h>
+
+// SERVOS
+Servo rightArmServo;
+Servo rightHandServo;
+Servo headServo;
+Servo leftArmServo;
+
+// LEDS NUMBERS
+int ledPin1 = 5;
+int ledPin2 = 6;
+
+// DELAY NUMBER
+int delayTime = 500;
+
+// SERVOS NUMBERS
+float servoNum[] = {0,0,0};
+float servoNum1 = 0;
+float servoNum2 = 0;
+float servoNum3 = 0;
+
+void setup()
+{
+  // SERIAL
+	Serial.begin(9600);
+
+  // RIGHT ARM
+  rightArmServo.attach(12);
+
+  // RIGHT HAND
+  rightHandServo.attach(3);
+
+  // LEFT ARM
+  leftArmServo.attach(2);
+
+  // HEAD
+  headServo.attach(4);
+
+  // WRITE FOR SERVOS
+  rightArmServo.write(180);
+  rightHandServo.write(70);
+  leftArmServo.write(180);
+  headServo.write(50);
+
+  // DELAY
+  delay(1000);
+
+  // TWO LEDS
+	pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+}
+
+void loop()
+{
+  // TWO LEDS
+  digitalWrite(ledPin1, HIGH);
+  digitalWrite(ledPin2, HIGH);
+  ////////////////////////////
+
+  // SERVOS ARE RUNNING
+  if (servoNum1 < -130)
+  {
+    if (servoNum2 < -130)
+    {
+      servoNum2 = -131;
+      delay(1000);
+      servoNum1 = 0;
+      servoNum2 = 0;
+    }
+    else
+    {
+      servoNum1 = -131;
+      servoNum2 -= 0.8;
+
+      rightArmServo.write(50 - servoNum2);
+      leftArmServo.write(50 - servoNum2);
+
+      Serial.println(servoNum2);
+    }
+  }
+  else
+  {
+    servoNum1 -= 0.8;
+
+    rightArmServo.write(180 + servoNum1);
+    leftArmServo.write(180 + servoNum1);
+
+    Serial.println(servoNum1);
+  }
+}
+
