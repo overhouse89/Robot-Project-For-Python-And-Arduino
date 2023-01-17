2ab5fd2f84375a0b2996a7a4b1c194c005467cd0
Maarit <ylitalo.maarit@gmail.com>
2023-01-17 18:15:52 +0200

    New codes are added

--- modules/robot_tkinter.py
+++ modules/robot_tkinter.py
@@ -1,3 +1,24 @@
 
-def robot_tkinter():
-    pass
+# Tkinter
+import tkinter
+
+def tkinter_head():
+    # Tkinter
+    tk_head = tkinter.Tk()  # Screen
+
+    # Graphic
+    tk_head.geometry("800x600")  # Screen's size
+    tk_head.title("Dancing Robot")  # Screen's title
+
+    # Buttons
+    button_for_head = tkinter.Button(tk_head, text ="Head", width=15)  # Head button
+    button_for_left_arm = tkinter.Button(tk_head, text ="Left Arm", width=15)  # Left arm button
+    button_for_right_arm = tkinter.Button(tk_head, text ="Right Arm", width=15)  # Right arm button
+
+    # Packs
+    button_for_head.pack()
+    button_for_left_arm.pack()
+    button_for_right_arm.pack()
+
+    # Loop
+    tk_head.mainloop()  # Screen is showing
--- robot_for_python.py
+++ robot_for_python.py
@@ -1,32 +1,15 @@
-# Robot for Python
 
 # Imports
 import serial
 import time
 
 # Modules
-from modules import robot_body
-from modules import robot_tkinter
-
-# Select serial's address
-select_serial_input = input("Address: ").strip()
-print("Waiting..")
-time.sleep(2)
+from modules import robot_body  # All robot's body
+from modules import robot_tkinter  # Tkinter for robot's head
 
 try:
 	serial_connection = serial.Serial("/dev/cu.HC-06")  # Bluetooth is connected
-	serial_connection.write(b's')
-	serial_connection.write(b'k')
-	while True:
-		body_input = input("BODY: ").strip()  # Robot's body
-		if body_input == 'a':
-			serial_connection.write(b'a')  # Robot's head is turning left
-		elif body_input == 's':
-			serial_connection.write(b's')  # Robot's head is turning center
-		elif body_input == 'd':
-			serial_connection.write(b'd')  # Robot's head is turning right
 except Exception:
-	# Disconnected
-	print(select_serial_input,"isn't connected.")
+	print("Arduino isn't connected.")  # Disconnected
 
-
+robot_tkinter.tkinter_head()  # Tkinter
