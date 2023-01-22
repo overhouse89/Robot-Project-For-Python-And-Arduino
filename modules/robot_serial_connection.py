39a48e43b0df8ee2f1534317afb4104b3e0d9e38
Maarit Ylitalo <ylitalo.maarit@gmail.com>
2023-01-22 21:33:32 +0200

    New code is added

--- modules/robot_serial_connection.py
+++ modules/robot_serial_connection.py
@@ -3,25 +3,23 @@
 import serial  # Serial's address
 import time  # Waiting
 
-# Modules
-#from modules import robot_body
-
 # Variables
 serial_arduino = ""
 
-# Python's code is searching serial's address
+#Python's code is searching serial's address
 try:
-    # Serial's address is connected
-	serial_connection = serial.Serial("/dev/cu.usbserial-1410")  # Bluetooth is connected
-	time.sleep(2)
-	serial_connection.write(b'b')
-	time.sleep(2)
-	serial_connection.write(b'a')
+	# Serial's address is connected
+	#serial_connection = serial.Serial("/dev/ttyUSB0")  # USB is connected  For Raspberry Pi
+	serial_connection = serial.Serial("/dev/rfcomm0")  # Bluetooth is connected  For Raspberry Pi
+	# Write line  --->   sudo rfcomm connect 0 98:D3:31:F7:61:83  ---->  for Raspberry Pi's terminal
 	serial_arduino = "Connected"
-	print("Connected")
+	print("connected")
+	time.sleep(2)
+	serial_connection.write(b'No Head')
 	serial_connection.close()
 except Exception:
-    # Serial's address is not connected
+	# Serial's address is not connected
 	time.sleep(2)  # Waiting for next line
 	serial_arduino = "Disconnected"
+	print("Disconnected")
 	
