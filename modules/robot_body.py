d3ffbaa9f2da23d878cc7b7a470569f39f8023f4
Maarit Ylitalo <ylitalo.maarit@gmail.com>
2023-01-22 21:28:09 +0200

    New code is added

--- modules/robot_body.py
+++ modules/robot_body.py
@@ -1,32 +1,31 @@
 
-# Bools
-button_head_off = True
-button_left_off = True
-button_right_off = True
+# Imports
+import serial  # Serial's address
+import time  # Waiting
+
+# buttons for loop
+def while_buttons(button_name):
+    serial_connection = serial.Serial("/dev/rfcomm0")  # Bluetooth is connected
+    #serial_connection = serial.Serial("/dev/ttyUSB0")
+    if button_name == "Head":
+        while True:
+            time.sleep(0.5)
+            serial_connection.write(b'Head')
+            time.sleep(6.8)
+            serial_connection.write(b'No Head')
+            print("OK")
+            break
 
 # Robot's head
 def body_head():
-    print("Head")
-    global button_head_off
-    if button_head_off:
-        button_head_off = False
-    else:
-        button_head_off = True
+    while_buttons("Head")
 
 # Robot's left arm
 def body_left_arm():
     print("Left Arm")
-    global button_left_off
-    if button_left_off:
-        button_left_off = False
-    else:
-        button_left_off = True
+    #while_buttons("Left Arm")
 
 # Robot's right arm
 def body_right_arm():
     print("Right Arm")
-    global button_right_off
-    if button_right_off:
-        button_right_off = False
-    else:
-        button_right_off = True
+    #while_buttons("Right Arm")
